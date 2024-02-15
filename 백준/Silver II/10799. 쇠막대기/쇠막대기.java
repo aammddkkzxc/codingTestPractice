5b41e758098b94441a7fc62f67367abf157bd709
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2024-02-15 21:36:02 +0900

    [Silver II] Title: 쇠막대기, Time: 376 ms, Memory: 22788 KB -BaekjoonHub

---
+++ 백준/Silver II/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<Character>();
+        String value = scanner.nextLine();
+        int answer = 0;
+        
+        for(int i = 0; i < value.length(); i++) {
+            if(value.charAt(i) == '(') {
+                stack.push('(');
+            }
+            if(value.charAt(i) == ')') {
+                stack.pop();
+                if (value.charAt(i - 1) == '(') {
+                    answer += stack.size();
+                }
+                if (value.charAt(i - 1) == ')') {
+                    answer += 1;
+                }
+            }
+        }
+        
+        System.out.println(answer);
+    }
+}
