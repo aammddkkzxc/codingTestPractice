a759a3158c711f051705173412a4c8a298b177d7
aammddkkzxc <97333563+aammddkkzxc@users.noreply.github.com>
2024-06-01 23:48:51 +0900

    [level 2] Title: N개의 최소공배수, Time: 3547.18 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        
+        if(arr.length == 1){
+            return arr[0];
+        }
+        
+        int a = arr[0];
+        int gcd = 0;
+        
+        for(int i = 1; i < arr.length; i++){
+            int b = arr[i];
+            for(int j = a*b; j > 0; j--){
+                if((j%a == 0) && (j%b == 0))
+                    gcd = j;
+            }
+            a = gcd;
+        }
+        
+        return gcd;
+    }
+}
